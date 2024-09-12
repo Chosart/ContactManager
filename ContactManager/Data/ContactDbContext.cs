12a57d813b818934d33f5faae0ac2fbde069ae88
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-12 21:57:12 +0300

    Connect do Database and set up ContactDbContext with Fluent API configuration

---
+++ ContactManager/Data/ContactDbContext.cs
@@ -0,0 +1,46 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Data
+{
+    public class ContactDbContext : DbContext
+    {
+        public ContactDbContext(DbContextOptions<ContactDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Contact>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            modelBuilder.Entity<Contact>()
+               .Property(c => c.Email)
+               .IsRequired()
+               .HasMaxLength(200);
+
+            modelBuilder.Entity<Contact>()
+                .HasOne(c => c.Category)
+                .WithMany(cat => cat.Contacts)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Category>()
+               .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Category>()
+                .Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+    }
+}
