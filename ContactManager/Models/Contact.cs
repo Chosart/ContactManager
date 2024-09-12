3daf3a853db1b733754d05673a6e8f7bed1237e1
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-12 20:14:35 +0300

    Implement Contact and Catogory classes in the Models folder

---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        //Foreign Key
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
