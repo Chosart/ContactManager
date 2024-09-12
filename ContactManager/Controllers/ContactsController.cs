40f082c42701135bc3adab51d6d9d76077027910
Chosart <164737726+Chosart@users.noreply.github.com>
2024-09-12 22:22:51 +0300

    Add GET initilisation in ContactsController

---
+++ ContactManager/Controllers/ContactsController.cs
@@ -0,0 +1,41 @@
+﻿using ContactManager.Data;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactDbContext _context;
+
+        public ContactsController(ContactDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetContacts()
+        {
+            return await _context.Contacts.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contact>> GetContact(int id)
+        {
+            var contact = await _context.Contacts.FindAsync(id);
+
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(contact);
+        }
+
+
+    }
+}
